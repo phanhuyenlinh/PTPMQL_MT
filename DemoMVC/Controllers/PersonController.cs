c32da01f35ebfc735c9dd49ca88f02a7c69dda79
phanhuyenlinh <Huyenlinhh16042002@gmail.com>
2024-08-27 14:30:30 +0700

    Update_Code

---
+++ DemoMVC/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using DEMOMVC.Models;
+namespace DEMOMVC.Controllers{
+    public class PersonController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+        [HttpPost]
+                public IActionResult Index (Person ps)
+        {
+            string strResult = "Xin chao" + ps.PersonId + "_" + ps.FullName + "_" + ps.Address;
+            ViewBag.inforPerson = strResult;
+            return View();
+        }
+
+    }
+}
